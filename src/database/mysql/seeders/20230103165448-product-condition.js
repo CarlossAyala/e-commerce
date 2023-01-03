c7525c5f45fb10deac3c0b262e6824d53d0e8160
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-03 17:14:10 -0300

    feat: seeder for product-condition addeed

--- src/database/mysql/seeders/20230103165448-product-condition.js
+++ src/database/mysql/seeders/20230103165448-product-condition.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
 const { ProductCondition } = require('../models');
 
 module.exports = {
@@ -13,7 +14,30 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
      */
-    await queryInterface.bulkInsert(ProductCondition.tableName, [], {});
+    await queryInterface.bulkInsert(ProductCondition.tableName, [
+      {
+        id: uuidv4(),
+        name: 'Nuevo',
+        description: 'Este producto nunca ha sido usado antes.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Usado',
+        description: 'Este producto ha sido utilizado previamente.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Reacondicionado',
+        description:
+          'Este producto contiene piezas que fueron reemplazadas y testeadas para que funcione y luzca como nuevo.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   async down(queryInterface) {
