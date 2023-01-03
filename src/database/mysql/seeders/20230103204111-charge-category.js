575f79e4ea075fbb44fd2d59b1ffa47e30138c95
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-03 17:49:03 -0300

    feat: seeder for charge-category added

---
+++ src/database/mysql/seeders/20230103204111-charge-category.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const { ChargeCategory } = require('../models');
+
+module.exports = {
+  async up(queryInterface) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(ChargeCategory.tableName, [
+      {
+        id: uuidv4(),
+        name: 'Impuestos',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Tarifas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(ChargeCategory.tableName, null, {});
+  },
+};
