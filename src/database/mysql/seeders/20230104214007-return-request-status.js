8654d9d19b79413f90fdb481333ab62a47c76a94
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-04 18:50:20 -0300

    feat: seeder for return-request-status added

---
+++ src/database/mysql/seeders/20230104214007-return-request-status.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const { ReturnRequestStatus } = require('../models');
+
+module.exports = {
+  async up(queryInterface) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(ReturnRequestStatus.tableName, [
+      {
+        id: uuidv4(),
+        name: 'Iniciado',
+        description:
+          'La solicitud de devolución ha sido enviada por el cliente.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'En curso',
+        description:
+          'La solicitud de devolución está siendo revisada por el vendedor.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Aprobado',
+        description:
+          'La devolución del producto ha sido aprobada y se ha realizado el reembolso al cliente.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Rechazado',
+        description:
+          'La solicitud de devolución ha sido rechazada por el vendedor.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Cancelado',
+        description:
+          'La solicitud de devolución ha sido cancelada por el cliente.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(ReturnRequestStatus.tableName, null, {});
+  },
+};
