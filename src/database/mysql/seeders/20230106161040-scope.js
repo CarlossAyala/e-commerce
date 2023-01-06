98e85a7b958767567d5420c54863d851abfc0ec9
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-06 13:48:44 -0300

    feat: seeder for scope added

---
+++ src/database/mysql/seeders/20230106161040-scope.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const { Scope } = require('../models');
+
+const { system, store, mix } = Scope.enums;
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(Scope.tableName, [
+      {
+        id: uuidv4(),
+        name: 'Security System Manager',
+        description:
+          'Persona encargada de gestionar los Roles, Scopes y Permisos del E-Commerce.',
+        type: system,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Security Store Manager',
+        description:
+          'Persona encargada de gestionar los Roles, Scopes y Permisos de una Tienda.',
+        type: system,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Category Manager',
+        description:
+          'Persona encargada de gestionar las categorías del E-Commerce.',
+        type: system,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Brand Verification Manager',
+        description:
+          'Persona encargada de verificar si una Tienda del E-Commerce es Oficial.',
+        type: system,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Manager',
+        description:
+          'Persona encargada de gestionar los productos de una tienda.',
+        type: store,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Returns Manager',
+        description:
+          'Persona encargada de gestionar los pedidos de devolución de productos de una tienda.',
+        type: store,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Questions Manager',
+        description:
+          'Persona encargada de gestionar las preguntas y respuestas sobre los productos de una tienda.',
+        type: store,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Review Manager',
+        description:
+          'Persona encargada de analizar las reseñas de los productos de una tienda.',
+        type: store,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete(Scope.tableName, null, {});
+  },
+};
