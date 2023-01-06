5bc96dfae905412ec2815084f2b99994b21d108a
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-05 22:04:31 -0300

    feat: seeder for scope-type added

---
+++ src/database/mysql/seeders/20230105235919-scope-type.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const { ScopeType } = require('../models');
+
+module.exports = {
+  async up(queryInterface) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(ScopeType.tableName, [
+      {
+        id: uuidv4(),
+        name: 'Category Manager',
+        description:
+          'Persona encargada de gestionar las categorías del E-Commerce.',
+        type: 'e-commerce',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Brand Verification Manager',
+        description:
+          'Persona encargada de verificar si una tienda del E-Commerce es una Tienda Oficial.',
+        type: 'e-commerce',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Manager',
+        description:
+          'Persona encargada de gestionar los productos de una tienda.',
+        type: 'seller',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Returns Manager',
+        description:
+          'Persona encargada de gestionar los pedidos de devolución de productos de una tienda.',
+        type: 'seller',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Questions Manager',
+        description:
+          'Persona encargada de gestionar las preguntas y respuestas sobre los productos de una tienda.',
+        type: 'seller',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: 'Product Review Manager',
+        description:
+          'Persona encargada de analizar las reseñas de los productos de una tienda.',
+        type: 'seller',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete(ScopeType.tableName, null, {});
+  },
+};
