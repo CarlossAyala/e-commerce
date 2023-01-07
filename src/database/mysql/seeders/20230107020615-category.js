3f369afb8da5bb6201871cc2bf346ac0968d80b9
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-07 00:55:45 -0300

    feat: seeder for category added and product-condition seeder was deleted

---
+++ src/database/mysql/seeders/20230107020615-category.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const { v4: uuidv4 } = require('uuid');
+const { faker } = require('@faker-js/faker/locale/es_MX');
+const { Category } = require('../models');
+
+const createRandomCategory = (parentId = null) => {
+  // https://fakerjs.dev/api/helpers.html#unique
+  const name = faker.helpers.unique(faker.commerce.department);
+
+  return {
+    id: uuidv4(),
+    name,
+    available: true,
+    parent_id: parentId,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+};
+
+const generateNCategories = (n = 1, parentCategory = {}) => {
+  const categories = [];
+  for (let i = 1; i <= n; i++) {
+    const category = createRandomCategory(parentCategory?.id);
+    categories.push(category);
+  }
+
+  return categories;
+};
+
+module.exports = {
+  async up(queryInterface) {
+    const NUM_MAIN_CATEGORIES = 3;
+    const NUM_SUB_CATEGORIES = 5;
+
+    // Generate mains Categories
+    const categories = generateNCategories(NUM_MAIN_CATEGORIES);
+    await queryInterface.bulkInsert(Category.tableName, categories);
+
+    // Generate sub-categories for each category
+    const subCategories = [];
+    for (const category of categories) {
+      const subCategory = generateNCategories(NUM_SUB_CATEGORIES, category);
+      subCategories.push(subCategory);
+    }
+
+    /* 
+      subCategories are a array of arrays, so we need to flat them for
+      bulkInsert method.
+      subCategories before flat: 
+      [ 
+        [{}, {}, {}],
+        [{}, {}, {}],
+        [{}, {}, {}]
+        ...
+      ] 
+      subCategories after flat:
+      [
+        {}, {}, {},
+        {}, {}, {},
+        {}, {}, {},
+        ...
+      ]
+    */
+    const flatedSubCategories = subCategories.flat();
+    await queryInterface.bulkInsert(Category.tableName, flatedSubCategories);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete(Category.tableName, null, {});
+  },
+};
