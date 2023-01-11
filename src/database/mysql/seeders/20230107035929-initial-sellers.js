1c54514808e3e858ce9b9c5f39dba3bb070c2fd6
Carloss Ayala <infocarlosayala@gmail.com>
2023-01-10 23:35:18 -0300

    fix: add new implentation of user-role

--- src/database/mysql/seeders/20230107035929-initial-sellers.js
+++ src/database/mysql/seeders/20230107035929-initial-sellers.js
@@ -4,17 +4,14 @@ const { v4: uuidv4 } = require('uuid');
 const { faker } = require('@faker-js/faker/locale/es_MX');
 const config = require('../../../config');
 const Encrypter = require('../../../utils/encrypter');
-const { User, Role } = require('../models');
+const { User, Role, UserRole } = require('../models');
 
-const createRandomSeller = async (roleId) => {
-  const password =
-    (await Encrypter.encrypt(config.seller.password)) ||
-    (await Encrypter.encrypt(email));
-
+const createRandomUsers = async () => {
   const userSex = faker.name.sex();
   const name = faker.name.firstName(userSex);
   const lastName = faker.name.lastName(userSex);
   const email = faker.helpers.unique(faker.internet.email, [name, lastName]);
+  const password = await Encrypter.encrypt(config.seller.password);
 
   return {
     id: uuidv4(),
@@ -22,20 +19,29 @@ const createRandomSeller = async (roleId) => {
     last_name: lastName,
     email,
     password,
-    fk_role: roleId,
     created_at: new Date(),
     updated_at: new Date(),
   };
 };
 
-const generateNSellers = async (n = 1, role = {}) => {
-  const sellers = [];
+const generateNUsers = async (n = 1) => {
+  const users = [];
   for (let i = 1; i <= n; i++) {
-    const seller = await createRandomSeller(role.id);
-    sellers.push(seller);
+    const user = await createRandomUsers();
+    users.push(user);
   }
 
-  return sellers;
+  return users;
+};
+
+const generateSeller = (user, role) => {
+  return {
+    id: uuidv4(),
+    fk_user: user.id,
+    fk_role: role.id,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
 };
 
 module.exports = {
@@ -50,17 +56,30 @@ module.exports = {
         },
       });
 
-      // Generate users with Seller Role
-      const sellers = await generateNSellers(NUM_SELLERS, sellerRole);
+      // Generate Users
+      const users = await generateNUsers(NUM_SELLERS);
 
-      // Create sellers
-      await queryInterface.bulkInsert(User.tableName, sellers);
+      // Generate Sellers
+      const sellers = [];
+      for (const user of users) {
+        const seller = generateSeller(user, sellerRole);
+        sellers.push(seller);
+      }
+
+      // Create users
+      await queryInterface.bulkInsert(User.tableName, users);
+      // Create users with Sellers Role
+      await queryInterface.bulkInsert(UserRole.tableName, sellers);
     } catch (error) {
       console.error(error);
     }
   },
 
   async down(queryInterface) {
+    // TODO: How to delete users and roles created above?
+    // maybe should creating a file with those at the moment were are created
+    // and then delete them by their id's
+    await queryInterface.bulkDelete(UserRole.tableName, null, {});
     await queryInterface.bulkDelete(User.tableName, null, {});
   },
 };
