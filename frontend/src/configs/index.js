2b36fd00e62a2d84974807a16b1b85a6b71c3bb4
Carlos Ayala <infocarlosayala@gmail.com>
2024-07-10 14:23:01 -0300

    fix: socket-io

--- frontend/src/configs/index.js
+++ frontend/src/configs/index.js
@@ -1,4 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 export const API_ECOMMERCE = `${API_URL}/e-commerce`;
 export const API_SELLER = `${API_URL}/seller`;
 export const API_SHARED = `${API_URL}/shared`;
--- frontend/src/shared/libs/socket.js
+++ frontend/src/shared/libs/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
-import { API_URL } from "../../configs";
+import { SOCKET_URL } from "../../configs";
 
-const Socket = io(API_URL, {
+const Socket = io(SOCKET_URL, {
   auth: {},
   autoConnect: false,
 });
