76ad3e72bb38760f9cca8a5e550c746e2fb0c3f0
Carlos Ayala <infocarlosayala@gmail.com>
2024-07-10 13:50:46 -0300

    feat: vercel deploy config

--- frontend/src/configs/index.js
+++ frontend/src/configs/index.js
@@ -1,6 +1,6 @@
 export const API_URL = import.meta.env.VITE_API_URL;
-export const API_ECOMMERCE = `${API_URL}/api/e-commerce`;
-export const API_SELLER = `${API_URL}/api/seller`;
-export const API_SHARED = `${API_URL}/api/shared`;
-export const API_AUTH = `${API_URL}/api/auth`;
-export const API_ADMIN = `${API_URL}/api/admin`;
+export const API_ECOMMERCE = `${API_URL}/e-commerce`;
+export const API_SELLER = `${API_URL}/seller`;
+export const API_SHARED = `${API_URL}/shared`;
+export const API_AUTH = `${API_URL}/auth`;
+export const API_ADMIN = `${API_URL}/admin`;
